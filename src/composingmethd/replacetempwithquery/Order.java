29652c66e3be929cb63afcde0ad55f4dfa504e5d
zhangxuefeng_jkcrm.cn_cloud-dsp <zhangxuefeng@jkcrm.cn>
2017-07-04 17:44:43 +0800

    Replace Temp With Query - 2

--- src/composingmethd/replacetempwithquery/Order.java
+++ src/composingmethd/replacetempwithquery/Order.java
@@ -10,12 +10,16 @@ public class Order {
 	private int _itemPrice;
 
 	double getPrice() {
-		final int basePrice = _quantity * _itemPrice;
+		final int basePrice = basePrice();
 		final double discountFactor;
 		if (basePrice > 1000)
 			discountFactor = 0.95;
 		else
 			discountFactor = 0.98;
 		return basePrice * discountFactor;
+	}
+
+	private int basePrice() {
+		return _quantity * _itemPrice;
 	}
 }
