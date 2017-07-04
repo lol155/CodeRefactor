4195ac4b5f6f573050504a18d654aca9f76ea432
zhangxuefeng_jkcrm.cn_cloud-dsp <zhangxuefeng@jkcrm.cn>
2017-07-04 17:21:23 +0800

    extract method 提炼函数 无局部变量

--- src/composingmethod/extractmethod/Printer.java
+++ src/composingmethod/extractmethod/Printer.java
@@ -16,20 +16,23 @@ public class Printer {
 		Enumeration e = _orders.elements();
 		double outstanding = 0.0;
 		
-		// print banner
-		System.out.println("**************************");
-		System.out.println("***** Customer Owes ******");
-		System.out.println("**************************");
+		printBanner();
 		
 		// calculate outstanding
 		while (e.hasMoreElements()) {
 			Order each = (Order) e.nextElement();
 			outstanding += each.getAmount();
 		}
-		
+
 		// print details
 		System.out.println("name:" + _name);
 		System.out.println("amount" + outstanding);
+	}
+
+	private void printBanner() {
+		System.out.println("**************************");
+		System.out.println("***** Customer Owes ******");
+		System.out.println("**************************");
 	}
 }
 
